ef2ec383033577c69c98958f7c7f25568ab253d7
zephyr520 <zephyr_520@163.com>
2020-10-25 14:46:25 +0800

    编写helloworld示例

---
+++ foodie-api/src/main/java/com/zephyr/FoodieApiApplication.java
@@ -0,0 +1,12 @@
+package com.zephyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieApiApplication.class, args);
+    }
+}
